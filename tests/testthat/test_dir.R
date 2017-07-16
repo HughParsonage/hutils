faaed598b9bb9f81cf876a96b95f6ed26b132a36
HughParsonage <hugh.parsonage@gmail.com>
2017-07-16 13:22:24 +1000

    provide.dir should not error if exists

--- tests/testthat/test_dir.R
+++ tests/testthat/test_dir.R
@@ -4,4 +4,5 @@ test_that("Directory created", {
   TempDir <- tempdir()
   provide.dir(file.path(TempDir, "XX"))
   expect_true(dir.exists(file.path(TempDir, "XX")))
+  provide.dir(file.path(TempDir, "XX"))
 })
