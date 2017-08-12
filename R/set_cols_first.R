ec88e83c4085f52c76798e4412b2209a8c00e857
HughParsonage <hugh.parsonage@gmail.com>
2017-08-13 00:52:57 +1000

    Add set_colsuborder

--- R/set_cols_first.R
+++ R/set_cols_first.R
@@ -1,18 +1,24 @@
 #' Put columns first or last
 #' @description Reorder columns of a \code{data.table} (via \code{setcolorder}) so that particular columns 
-#' appear first (or last).
+#' appear first (or last), or in a particular order.
 #' @param DT A data.table.
-#' @param cols Character vector of columns to put before (after) all others.
+#' @param cols Character vector of columns to put before (after) all others or, in the case of \code{set_colsuborder},
+#' a vector of columns in the order requested.
 #' @param intersection Use the intersection of the names of \code{DT} and \code{cols}. If \code{FALSE} 
 #' any \code{cols} are not the names of \code{DT}, the function may error on behalf of \code{data.table}.
+#' Not availble for \code{set_colsuborder}.
+#' @details In the case of \code{set_colsuborder} the group of columns \code{cols} occupy the same positions
+#' in \code{DT} but in a different order. See examples.
 #' @examples 
-#' if (requireNamespace("data.table", quietly = TRUE)) {
-#'   library(data.table)
+#' 
+#' library(data.table)
 #'   
-#'   DT <- data.table(y = 1:5, z = 11:15, x = letters[1:5])
-#'   set_cols_first(DT, "x")
-#' }
-#' @export set_cols_first set_cols_last
+#' DT <- data.table(y = 1:5, z = 11:15, x = letters[1:5])
+#' set_cols_first(DT, "x")[]
+#' set_cols_last(DT, "x")[]
+#' set_colsuborder(DT, c("x", "y"))[]
+#' 
+#' @export set_cols_first set_cols_last set_colsuborder
 
 set_cols_first <- function(DT, cols, intersection = TRUE){
   if (intersection){
@@ -31,5 +37,29 @@ set_cols_last <- function(DT, cols, intersection = TRUE){
     return(setcolorder(DT, c(setdiff(names(DT), cols), cols)))
   }
 }
+
+#' @rdname set_cols_first
+set_colsuborder <- function(DT, cols, intersection = TRUE) {
+  orig_order <- names(DT)
+  
+  stopifnot(is.character(cols),
+            length(cols) > 0,
+            isTRUE(intersection),
+            all(cols %chin% orig_order))
+  
+  matches <- chmatch(orig_order, cols)
+  new_order <- orig_order
+  j <- 0
+  for (i in seq_along(new_order)) {
+    if (!is.na(matches[i])) {
+      j <- j + 1
+      new_order[i] <- cols[j]
+    }
+  }
+  
+  setcolorder(DT, new_order)
+}
+
+
 
 
--- tests/testthat/test_cols_first.R
+++ tests/testthat/test_cols_first.R
@@ -25,3 +25,12 @@ test_that("Colums out of intersection", {
   expect_error(set_cols_first(DT, c("x", "zz"), intersection = FALSE))
   expect_error(set_cols_last(DT, c("x", "zz"), intersection = FALSE))
 })
+
+test_that("Swaps columns appropriate", {
+  DT <- data.table(A = 1, B = 2, C = 3)
+  expect_equal(names(set_colsuborder(DT, c("C", "B"))), c("A", "C", "B"))
+  # Idempotent
+  expect_equal(names(set_colsuborder(DT, c("C", "B"))), c("A", "C", "B"))
+  DT <- data.table(A = 1, B = 2, C = 3)
+  expect_equal(names(set_colsuborder(DT, c("B", "C"))), c("A", "B", "C"))
+})
