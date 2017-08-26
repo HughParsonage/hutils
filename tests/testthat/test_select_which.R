dcd7181899e3bd567b3013afea4e7c495c1a661e
HughParsonage <hugh.parsonage@gmail.com>
2017-08-26 13:17:11 +1000

    Add test for coverage

--- tests/testthat/test_select_which.R
+++ tests/testthat/test_select_which.R
@@ -12,3 +12,12 @@ test_that("Returns correct columns", {
   
   expect_error(select_which(data.frame(x = 1), checkDT = TRUE), regexp = "must be a data.table")
 })
+
+test_that("Returns correct columns when .and.dots not char", {
+  library(data.table)
+  DT <- data.table(x = 1:5,
+                   y = letters[1:5],
+                   AB = c(NA, TRUE, FALSE, TRUE, FALSE))
+  out <- select_which(DT, anyNA, .and.dots = 2)
+  expect_equal(names(out), c("AB", "y"))
+})
