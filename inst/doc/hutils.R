bf45a7455e8e645c8b5fae6fb7818ef6eb7812f7
HughParsonage <hugh.parsonage@gmail.com>
2017-09-17 00:07:41 +1000

    Add inst/doc to github

---
+++ inst/doc/hutils.R
@@ -0,0 +1,138 @@
+## ----knitrOpts-----------------------------------------------------------
+library(knitr)
+suggested_packages <- c("geosphere", "nycflights13", "dplyr", "ggplot2", "microbenchmark")
+opts_chunk$set(eval = all(vapply(suggested_packages, requireNamespace, quietly = TRUE, FUN.VALUE = FALSE)))
+
+## ----loadPackages--------------------------------------------------------
+library(geosphere)
+library(nycflights13)
+library(dplyr, warn.conflicts = FALSE)
+library(ggplot2)
+library(microbenchmark)
+library(data.table, warn.conflicts = FALSE)
+library(magrittr)
+library(hutils, warn.conflicts = FALSE)
+
+## ----aliases-------------------------------------------------------------
+OR(OR(TRUE,
+      stop("Never happens")),  ## short-circuits
+   AND(FALSE,
+       stop("Never happens")))  
+
+## ----compare_if_else-----------------------------------------------------
+my_check <- function(values) {
+  all(vapply(values[-1], function(x) identical(values[[1]], x), logical(1)))
+}
+
+
+cnd <- sample(c(TRUE, FALSE, NA), size = 100e3, replace = TRUE)
+yes <- sample(letters, size = 100e3, replace = TRUE)
+no <- sample(letters, size = 100e3, replace = TRUE)
+na <- sample(letters, size = 100e3, replace = TRUE)
+
+microbenchmark(dplyr =  dplyr::if_else(cnd, yes, no, na),
+               hutils = hutils::if_else(cnd, yes, no, na),
+               check = my_check) %T>%
+  print %>%
+  autoplot
+
+cnd <- sample(c(TRUE, FALSE, NA), size = 100e3, replace = TRUE)
+yes <- sample(letters, size = 1, replace = TRUE)
+no <- sample(letters, size = 100e3, replace = TRUE)
+na <- sample(letters, size = 1, replace = TRUE)
+
+microbenchmark(dplyr =  dplyr::if_else(cnd, yes, no, na),
+               hutils = hutils::if_else(cnd, yes, no, na),
+               check = my_check) %T>%
+  print %>%
+  autoplot
+
+## ----compare_coalesce----------------------------------------------------
+x <- sample(c(letters, NA), size = 100e3, replace = TRUE)
+A <- sample(c(letters, NA), size = 100e3, replace = TRUE)
+B <- sample(c(letters, NA), size = 100e3, replace = TRUE)
+C <- sample(c(letters, NA), size = 100e3, replace = TRUE)
+
+microbenchmark(dplyr =  dplyr::coalesce(x, A, B, C),
+               hutils = hutils::coalesce(x, A, B, C),
+               check = my_check) %T>%
+  print %>%
+  autoplot
+
+## ----compare_coalesce_short_circuit_x------------------------------------
+x <- sample(c(letters), size = 100e3, replace = TRUE)
+
+microbenchmark(dplyr =  dplyr::coalesce(x, A, B, C),
+               hutils = hutils::coalesce(x, A, B, C),
+               check = my_check) %T>%
+  print %>%
+  autoplot
+
+## ----compare_coalesce_short_circuit_A------------------------------------
+x <- sample(c(letters, NA), size = 100e3, replace = TRUE)
+A <- sample(c(letters), size = 100e3, replace = TRUE)
+
+microbenchmark(dplyr =  dplyr::coalesce(x, A, B, C),
+               hutils = hutils::coalesce(x, A, B, C),
+               check = my_check) %T>%
+  print %>%
+  autoplot
+
+## ----canonical_drop_DT---------------------------------------------------
+DT <- data.table(A = 1:5, B = 1:5, C = 1:5)
+DT[, A := NULL]
+
+## ----drop_col_hutils-----------------------------------------------------
+DT <- data.table(A = 1:5, B = 1:5, C = 1:5)
+DT %>%
+  drop_col("A") %>%
+  drop_col("B")
+
+# or
+DT <- data.table(A = 1:5, B = 1:5, C = 1:5)
+DT %>%
+  drop_cols(c("A", "B"))
+
+## ----drop_colr-----------------------------------------------------------
+flights <- as.data.table(flights)
+
+flights %>%
+  drop_colr("time") %>%
+  drop_colr("arr(?!_delay)", perl = TRUE)
+
+## ----drop_empty_cols-----------------------------------------------------
+planes %>% 
+  as.data.table %>% 
+  .[!complete.cases(.)]
+
+planes %>% 
+  as.data.table %>% 
+  .[!complete.cases(.)] %>% 
+  # drops speed
+  drop_empty_cols
+
+## ----duplicated_rows-----------------------------------------------------
+flights %>%
+  # only the 'second' of the duplicates is returned
+  .[duplicated(., by = c("origin", "dest"))]  
+
+flights %>%
+  # Both rows are returned and (by default)
+  # duplicates are presented adjacently
+  duplicated_rows(by = c("origin", "dest"))
+
+## ----haversine_distance--------------------------------------------------
+DT1 <- data.table(lat_orig = runif(1e5, -80, 80),
+                 lon_orig = runif(1e5, -179, 179),
+                 lat_dest = runif(1e5, -80, 80),
+                 lon_dest = runif(1e5, -179, 179))
+
+DT2 <- copy(DT1)
+
+microbenchmark(DT1[, distance := haversine_distance(lat_orig, lon_orig,
+                                                    lat_dest, lon_dest)],
+               
+               DT2[, distance := distHaversine(cbind(lon_orig, lat_orig),
+                                               cbind(lon_orig, lat_orig))])
+rm(DT1, DT2)
+
