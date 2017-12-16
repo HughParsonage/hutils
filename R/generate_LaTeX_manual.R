1f867a1d79531106550fc684911cf3b97b4d4ce4
HughParsonage <hugh.parsonage@gmail.com>
2017-12-16 17:18:31 +1100

    Add generate LaTeX manual utility

---
+++ R/generate_LaTeX_manual.R
@@ -0,0 +1,13 @@
+#' Generate LaTeX manual of installed package
+#' @param pkg Quoted package name (must be installed).
+#' @return See \code{\link[base]{system}}.
+#' Called for its side-effect: creates a PDF in the current working directory.
+#' @source <https://stackoverflow.com/a/30608000/1664978>
+#' @export
+
+generate_LaTeX_manual <- function(pkg) {
+  pack <- pkg
+  path <- find.package(pack)
+  system(paste(shQuote(file.path(R.home("bin"), "R")),
+               "CMD", "Rd2pdf", shQuote(path)))
+}
