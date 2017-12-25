a8911cd83203d3c0150643990b7c50b8374c069f
HughParsonage <hugh.parsonage@gmail.com>
2017-12-25 19:19:55 +1100

    Provide option for viewer launch, not on Travis

--- R/generate_LaTeX_manual.R
+++ R/generate_LaTeX_manual.R
@@ -1,14 +1,15 @@
 #' Generate LaTeX manual of installed package
 #' @param pkg Quoted package name (must be installed).
+#' @param launch Should the PDF created be launched using the viewer (\code{TRUE} by default)?
 #' @return See \code{\link[base]{system}}.
 #' Called for its side-effect: creates a PDF in the current working directory. Requires a TeX distribution.
 #' @source \url{https://stackoverflow.com/a/30608000/1664978}
 #' @export
 
-generate_LaTeX_manual <- function(pkg) {
+generate_LaTeX_manual <- function(pkg, launch = TRUE) {
   pack <- pkg
   path <- find.package(pack)
   system(paste(shQuote(file.path(R.home("bin"), "R")),
-               "CMD", "Rd2pdf", shQuote(path)),
+               "CMD", "Rd2pdf", "--batch", if (!launch) "--no-preview", shQuote(path)),
          show.output.on.console = FALSE)
 }
--- tests/testthat/test_generate_LaTeX_manual.R
+++ tests/testthat/test_generate_LaTeX_manual.R
@@ -4,7 +4,7 @@ test_that("Generate LaTeX manual", {
   skip_on_cran()
   skip_if_not(!nzchar(Sys.getenv("texi2dvi")))
   skip_if_not(!file.exists("fastmatch.pdf"))
-  generate_LaTeX_manual("fastmatch")
+  generate_LaTeX_manual("fastmatch", launch = FALSE)
   expect_true(file.exists("fastmatch.pdf"))
   if (file.exists("fastmatch.pdf")) {
     file.remove("fastmatch.pdf")
