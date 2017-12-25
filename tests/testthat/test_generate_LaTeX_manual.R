1e14ddc4258dfc29812094939d700f7e36e1385f
HughParsonage <hugh.parsonage@gmail.com>
2017-12-25 17:55:03 +1100

    generate LaTeX manual required TeX

--- tests/testthat/test_generate_LaTeX_manual.R
+++ tests/testthat/test_generate_LaTeX_manual.R
@@ -2,6 +2,7 @@ context("LaTeX manual")
 
 test_that("Generate LaTeX manual", {
   skip_on_cran()
+  skip_if_not(!nzchar(Sys.getenv("texi2dvi")))
   skip_if_not(!file.exists("fastmatch.pdf"))
   generate_LaTeX_manual("fastmatch")
   expect_true(file.exists("fastmatch.pdf"))
