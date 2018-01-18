79889ceead95e0200155e4c16f44f0060cced128
HughParsonage <hugh.parsonage@gmail.com>
2018-01-19 00:00:28 +1100

    Fix namespace dependencies

--- R/hutils-package.R
+++ R/hutils-package.R
@@ -3,6 +3,7 @@
 #' @description Provides utility functions for, and drawing on, the 'data.table' package. The package also collates useful miscellaneous functions extending base R not available elsewhere. The name is a portmanteau of 'utils' and the author.
 #' @import data.table
 #' @importFrom magrittr %>%
+#' @importFrom magrittr not
 #' @importFrom fastmatch %fin%
 #' @importFrom fastmatch fmatch
 NULL
