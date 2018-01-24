e892c7ea37cc59457c453a98e523f1a0398871c9
HughParsonage <hugh.parsonage@gmail.com>
2018-01-24 22:23:46 +1100

    Skip LaTeX on appveyor

--- tests/testthat/test_generate_LaTeX_manual.R
+++ tests/testthat/test_generate_LaTeX_manual.R
@@ -2,6 +2,7 @@ context("LaTeX manual")
 
 test_that("Generate LaTeX manual", {
   skip_on_cran()
+  skip_on_appveyor()
   skip_if_not(!nzchar(Sys.getenv("texi2dvi")))
   skip_if_not(!file.exists("fastmatch.pdf"))
   generate_LaTeX_manual("fastmatch", launch = FALSE)
