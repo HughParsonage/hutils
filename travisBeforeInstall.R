6b1b83584841f14e33a53e8fa3cfa06e265a15f0
HughParsonage <hugh.parsonage@gmail.com>
2018-05-04 21:59:26 +1000

    Require new package (early version may be cached)

--- travisBeforeInstall.R
+++ travisBeforeInstall.R
@@ -1,6 +1,7 @@
 if (getRversion() >= "3.6") {
   # dev changes to ALTREP mean release is broken
-  if (!requireNamespace("data.table", quietly = TRUE)) {
+  if (!requireNamespace("data.table", quietly = TRUE) && 
+      package_version("data.table") >= "1.11.0") {
     install.packages("data.table")
   }
   data.table::update.dev.pkg()
