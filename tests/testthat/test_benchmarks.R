e2d38601f1fd7e4f925223d65fcf4a19477be388
HughParsonage <hugh.parsonage@gmail.com>
2018-05-12 17:40:10 +1000

    Try quantile

--- tests/testthat/test_benchmarks.R
+++ tests/testthat/test_benchmarks.R
@@ -15,7 +15,6 @@ test_that("Benchmarks", {
     len10 <- microbenchmark(if_else(z, 1, 2), times = 10e3)
     len10 <- microbenchmark(if_else(z, 1, 2), times = 10e3)
   }
-  ifelse_median <- median(microbenchmark(ifelse(z, 1, 2))[["time"]])
   len10 <- microbenchmark(if_else(z, 1, 2))
   len10 <- microbenchmark(if_else(z, 1, 2))
   expect_lt(median(len10$time), ifelse_median)
