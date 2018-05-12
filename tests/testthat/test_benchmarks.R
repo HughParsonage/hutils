a60090f9d828f843779e9251932baa5cfade90c8
HughParsonage <hugh.parsonage@gmail.com>
2018-05-12 16:07:17 +1000

    Weaker benchmark

--- tests/testthat/test_benchmarks.R
+++ tests/testthat/test_benchmarks.R
@@ -6,9 +6,16 @@ test_that("Benchmarks", {
   library(microbenchmark)
   invisible(gc(FALSE, reset = TRUE, full = TRUE))
   z <- as.logical(1:10 %% 3)
+  ifelse_median <- median(microbenchmark(ifelse(z, 1, 2))[["time"]])
   len10 <- microbenchmark(if_else(z, 1, 2))
   len10 <- microbenchmark(if_else(z, 1, 2))
-  expect_lt(median(len10$time), 4500)
+  if (median(len10$time) > ifelse_median) {
+    print(len10)
+  }
+  ifelse_median <- median(microbenchmark(ifelse(z, 1, 2))[["time"]])
+  len10 <- microbenchmark(if_else(z, 1, 2))
+  len10 <- microbenchmark(if_else(z, 1, 2))
+  expect_lt(median(len10$time), ifelse_median)
   
   z <- as.logical(1:1e5 %% 3)
   len10 <- microbenchmark(if_else(z, 1:1e5, 1:1e5))
