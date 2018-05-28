3349c74f5e1474674a9ab5aa33ad9f7ecd3d4e28
HughParsonage <hugh.parsonage@gmail.com>
2018-05-28 14:36:02 +1000

    Find pattern bug fixes (#10)
    * Add test for formal coverage
    
    * Fix find_pattern_in, add comment controls
    
    * [Travis]
    
    * Add all test
    
    * No coverage when not on windows

--- R/find_pattern.R
+++ R/find_pattern.R
@@ -4,6 +4,7 @@
 #' @param dir_recursive (logical, default: \code{TRUE}) Search within subdirectories of \code{basedir}?
 #' @param reader A function, akin to \code{base::readLines}, the default, that accepts a filename and returns a character vector.
 #' @param include.comments If \code{FALSE}, the default, comments (i.e. anything after a \code{\#}) are not searched.
+#' @param comment.char If \code{include.comments} is \code{FALSE}, what character marks a comment character? By default, \code{NULL}, which sets the correct comment symbol for R and TeX files. 
 #' @param use.OS Use the operating system to determine file list. Only available on Windows. If it fails, a fall-back option
 #' (using \code{dir}) is used.
 #' @param file_pattern A regular expression passed to \code{list.files(pattern = file.ext)}.
@@ -13,7 +14,8 @@
 #' @param file_contents_fixed (logical, default: \code{FALSE}) Should \code{file_contents} 
 #' be interpreted as a \code{fixed} regex?
 #' @param file.ext A file extension passed to the operating system if \code{use.OS} is used.
-#' @return A \code{data.table}, one row per filename with a match, which includes the first line that matched.
+#' @param which_lines One of \code{"first"} and \code{"all"}. If \code{"first"} only the first match in any file is returned in the result; if \code{"all"}, all matches are.
+#' @return A \code{data.table}, showing the matches per file.
 #' @export
 
 find_pattern_in <- function(file_contents,
@@ -21,12 +23,33 @@ find_pattern_in <- function(file_contents,
                             dir_recursive = TRUE,
                             reader = readLines,
                             include.comments = FALSE,
+                            comment.char = NULL,
                             use.OS = FALSE,
                             file_pattern = "\\.(R|r)(nw|md)?$",
                             file_contents_perl = TRUE,
                             file_contents_fixed = FALSE,
-                            file.ext = NULL) {
-  .reader <- match.fun(reader)
+                            file.ext = NULL, 
+                            which_lines = c("first", "all")) {
+  ..reader <- match.fun(reader)
+  .reader <- function(x) {
+    out <- ..reader(x)
+    if (!include.comments) {
+      if (is.null(comment.char)) {
+        comment.char <-
+          switch(tools::file_ext(x),
+                 "R" = "[#]",
+                 "Rmd" = "[#]",
+                 "Rnw" = "[#%]",
+                 "tex" = "%",
+                 "#")
+      }
+      
+      out <- sub(paste0(comment.char, ".*$"), "", out, perl = TRUE)
+    }
+    out
+  }
+  
+  
   if (length(file.ext)) {
     stopifnot(length(file.ext) == 1L,
               is.character(file.ext))
@@ -79,34 +102,68 @@ find_pattern_in <- function(file_contents,
               fixed = file_contents_fixed))
   }
   
+  which_lines <- match.arg(which_lines)
   matched_line_no <- function(x, .pattern = file_contents) {
-    grep(.pattern,
-         x = x,
-         perl = file_contents_perl,
-         fixed = file_contents_fixed)[1L]
+    switch(which_lines,
+           "first" = {
+             grep(.pattern,
+                  x = x,
+                  perl = file_contents_perl,
+                  fixed = file_contents_fixed)[1L]
+           }, 
+           "all" = {
+             grep(.pattern,
+                  x = x,
+                  perl = file_contents_perl,
+                  fixed = file_contents_fixed)
+           })
   }
   
   matched_pattern <- function(x, .pattern = file_contents) {
-    grep(.pattern,
-         x = x,
-         value = TRUE,
-         perl = file_contents_perl,
-         fixed = file_contents_fixed)[1L]
+    switch(which_lines,
+           "first" = {
+             grep(.pattern,
+                  x = x,
+                  value = TRUE,
+                  perl = file_contents_perl,
+                  fixed = file_contents_fixed)[1L]
+           }, 
+           "all" = {
+             grep(.pattern,
+                  x = x,
+                  value = TRUE,
+                  perl = file_contents_perl,
+                  fixed = file_contents_fixed)
+           })
   }
   
   has_pattern <- vapply(all_lines, contains_pattern, FALSE)
   if (any(has_pattern)) {
-    first_line_nos <- vapply(all_lines[has_pattern], matched_line_no, integer(1L))
-    first_lines <- vapply(all_lines[has_pattern], matched_pattern, character(1L))
-    data.table(file = R_files[has_pattern],
-               line_no = first_line_nos,
-               lines = first_lines)
+    switch(which_lines, 
+           "first" = {
+             first_line_nos <- vapply(all_lines[has_pattern], matched_line_no, integer(1L))
+             first_lines <- vapply(all_lines[has_pattern], matched_pattern, character(1L))
+             data.table(file = R_files[has_pattern],
+                        line_no = first_line_nos,
+                        lines = first_lines)
+           }, 
+           "all" = {
+             lapply(which(has_pattern), function(d) {
+               data.table(file = R_files[d], 
+                          line_no = matched_line_no(all_lines[[d]]), 
+                          lines = matched_pattern(all_lines[[d]]))
+             }) %>%
+               rbindlist
+           })
+    
   } else {
     data.table()
   }
-        }
-        
+}
+  
+#nocov start
 deprecate_windows_finder <- function() {
   if (identical(.Platform$OS.type,
-                "windows")) .Deprecated("find_pattern_in_windows", package = "hutilsInteractive")
+                "windows")) .Deprecated("hutilsInteractive::find_pattern_in_windows")
 }
+#nocov end
--- tests/testthat/test_find_pattern_in.R
+++ tests/testthat/test_find_pattern_in.R
@@ -123,4 +123,59 @@ test_that("On Windows", {
   expect_equal(unique(out[["line_no"]]), 1)
 })
 
+test_that("Multiples", {
+  tempd <- tempfile()
+  skip_if(dir.exists(tempd))
+  dir.create(tempd)
+  tempf <- tempfile(fileext = ".R", tmpdir = tempd)
+  writeLines(c("afpdom", "afp", "# afp"), 
+             tempf)
+  out <- find_pattern_in("afp", tempd, which_lines = "all")
+  expect_equal(nrow(out), 2)
+})
+
+
+test_that("include comments", {
+  skip_on_cran()
+  tempd <- tempfile()
+  skip_if(dir.exists(tempd))
+  dir.create(tempd)
+  temp.R <- tempfile(fileext = ".R", tmpdir = tempd)
+  writeLines(c("# This is an R script that doesn't contain fooc", 
+               "x <- 1", 
+               ""),
+             temp.R)
+  expect_equal(nrow(find_pattern_in("fooc", tempd)), 0)
+  
+  temp2.R <- tempfile(fileext = ".R", tmpdir = tempd)
+  writeLines(c("# This is an R script that does contain fooc", 
+               "fooc <- 1", 
+               ""),
+             temp2.R)
+  expect_equal(nrow(find_pattern_in("fooc", tempd)), 1)
+  
+  temp3.tex <- tempfile(fileext = ".tex", tmpdir = tempd)
+  writeLines(c("% File not containing fooc", 
+               "\\begin{document}", 
+               "food", 
+               "\\end{document}", 
+               ""), 
+             temp3.tex)
+  expect_equal(nrow(find_pattern_in("fooc", tempd, file_pattern = "(R|tex)$")), 1)
+  expect_equal(nrow(find_pattern_in("fooc",
+                                    tempd,
+                                    include.comments = TRUE,
+                                    file_pattern = "(R|tex)$")),
+               3)
+  expect_equal(nrow(find_pattern_in("fooc",
+                                    tempd,
+                                    include.comments = FALSE,
+                                    comment.char = "%", # i.e. keep R comments
+                                    file_pattern = "(R|tex)$")),
+               2)
+  
+  
+  
+})
+
 
