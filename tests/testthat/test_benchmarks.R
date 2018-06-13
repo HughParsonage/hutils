e28d99ec9ff183b5cc66c1d2b51200e4704ab488
HughParsonage <hugh.parsonage@gmail.com>
2018-06-14 00:48:08 +1000

    Add benchmarks too

--- tests/testthat/test_benchmarks.R
+++ tests/testthat/test_benchmarks.R
@@ -3,6 +3,7 @@ context("benchmarking")
 test_that("Benchmarks", {
   skip_on_cran()
   skip_if_not(identical(Sys.getenv("HUTILS_BENCHMARK"), "TRUE"))
+  skip_if_not_installed("microbenchmark")
   library(microbenchmark)
   invisible(gc(FALSE, reset = TRUE, full = TRUE))
   
@@ -16,6 +17,38 @@ test_that("Benchmarks", {
   len1e4c <- microbenchmark(if_else(z, "a", rep_len(letters, 1e4)))
   len1e4c_ifelse <- microbenchmark(base::ifelse(z, "a", rep_len(letters, 1e4)))
   expect_lt(median(len1e4c$time), 2e6)
+})
+
+test_that("selector faster than dt[, .]", {
+  skip_on_cran()
+  skip_if_not(identical(Sys.getenv("HUTILS_BENCHMARK"), "TRUE"))
+  skip_if_not_installed("microbenchmark")
+  dt <- setDT(list(x = sample.int(1e7), y = rnorm(1e7), z = as.character(runif(1e7))))
+  
+  library(microbenchmark)
+  invisible(gc(FALSE, reset = TRUE, full = TRUE))
+  
+  selector_0 <- microbenchmark(dt[, .(x, y)])
+  selector_1 <- microbenchmark(selector(dt, cols = c("x", "y")))
+  
+  expect_gt(median(selector_0$time), 
+            median(selector_1$time))
+  
+  dt <- dt[1:1e5]
+  
+  selector_0 <- microbenchmark(dt[, .(x, y)])
+  selector_1 <- microbenchmark(selector(dt, cols = c("x", "y")))
+  
+  expect_gt(median(selector_0$time), 
+            median(selector_1$time))
+  
+  dt <- dt[1:1e4]
+  
+  selector_0 <- microbenchmark(dt[, .(x, y)])
+  selector_1 <- microbenchmark(selector(dt, cols = c("x", "y")))
+  
+  expect_gt(median(selector_0$time), 
+            median(selector_1$time))
   
   
 })
