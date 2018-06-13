31218d9999815929f405afcc3a6cb31f55391dcb
HughParsonage <hugh.parsonage@gmail.com>
2018-06-14 00:07:17 +1000

    Add selector

---
+++ R/selector.R
@@ -0,0 +1,37 @@
+#' Fasting selection of \code{data.table} columns
+#' @description Present since \code{hutils 1.2.0}.
+#' 
+#' @param DT A \code{data.table}.
+#' @param ... Unquoted columns names.
+#' @param cols Character vector of column names.
+#' @param preserve.key Whether to reapply the key, if \code{DT} has one.
+#' 
+#' @return \code{DT} with the selected columns. 
+#' 
+#' @export
+
+
+selector <- function(DT, ..., cols = NULL, preserve.key = TRUE) {
+  dot_cols <- 
+    if (...length()) {
+      vapply(substitute(...()), deparse, character(1L))
+    }
+  
+  if (is.numeric(cols)) {
+    cols <- names(DT)[cols]
+  }
+  
+  if (is.null(dot_cols) && is.null(cols)) {
+    data.table()
+  }
+  
+  dt_key <- if (preserve.key) key(DT)
+  out <- setDT(lapply(c(dot_cols, cols), function(v) .subset2(DT, v)))
+  setnames(out, c(dot_cols, cols))
+  if (!is.null(dt_key) && all(dt_key %chin% c(dot_cols, cols))) {
+    setattr(out, "sorted", dt_key)
+  }
+  out
+}
+
+
---
+++ tests/testthat/test_1-2-0_selector.R
@@ -0,0 +1,18 @@
+context("selector")
+
+test_that("Expected results", {
+  dt <- data.table(x = 1, y = 2, z = 3, key = "z")
+  o1 <- selector(dt, x, y)
+  o2 <- selector(dt, cols = c("x", "y"))
+  o3 <- selector(dt, x, cols = "y")
+  o4 <- selector(dt, y, cols = "x")
+  expect_equal(o1, dt[, .(x, y)])
+  expect_equal(o2, dt[, .(x, y)])
+  expect_equal(o3, dt[, .(x, y)])
+  expect_equal(o4, dt[, .(y, x)])
+  expect_false(haskey(o4))
+  o5 <- selector(dt, z, x)
+  expect_equal(o5, dt[, .(z, x)])
+  expect_true(haskey(o5))
+  expect_equal(key(o5), "z")
+})
