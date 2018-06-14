75ca4cd2fc3568f2638586a8000d83f65516ff50
HughParsonage <hugh.parsonage@gmail.com>
2018-06-15 03:25:03 +1000

    Test instance where x[1] is NA

--- R/all_same_sign.R
+++ R/all_same_sign.R
@@ -8,20 +8,28 @@
 #' all_same_sign(1:10 - 1)
 #' all_same_sign(0)
 #' all_same_sign(NA)
+#' all_same_sign(c(NA, 1))
 #' all_same_sign("surprise?")
 #' all_same_sign(c(0, 0.1 + 0.2 - 0.3))
 #' 
 #' if (requireNamespace("microbenchmark", quietly = TRUE)) {
-#'   microbenchmark::microbenchmark(length(unique(sign(1:1e5), nmax = 3)) == 1L, 
-#'                                  all_same_sign(1:1e5))
+#'   library(microbenchmark)
+#'   microbenchmark(base = length(unique(sign(1:1e5), nmax = 3)) == 1L, 
+#'                  all_same_sign(1:1e5))
 #' }
+#' # Unit: microseconds
+#' #                   expr  min   lq mean median   uq  max neval cld
+#' #                   base 2012 2040 2322   2047 2063 9324   100   b
+#' # all_same_sign(1:1e+05)   86   86   94     89   93  290   100  a 
 #' 
 #' @export
 
 all_same_sign <- function(x) {
   OR(length(x) <= 1L,
      {
-       x1 <- x[1L]
+       if (anyNA(x1 <- x[1L])) {
+         return(NA)
+       }
        if (x1 == 0) {
          AND(min(x) == 0,
              max(x) == 0)
--- tests/testthat/test_1-2-0_all_same_sign.R
+++ tests/testthat/test_1-2-0_all_same_sign.R
@@ -5,6 +5,7 @@ test_that("all_same_sign", {
   expect_false(all_same_sign(1:10 - 1))
   expect_true(all_same_sign(0))
   expect_true(all_same_sign(NA))
+  expect_true(is.na(all_same_sign(c(NA, 1))))
   expect_true(all_same_sign(NULL))
   expect_true(all_same_sign("surprise?"))
   expect_false(xor(0.1 + 0.2 - 0.3 == 0, all_same_sign(c(0, 0.1 + 0.2 - 0.3))))
