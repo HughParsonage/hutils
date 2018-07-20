c33e4eba1197a053942fb3fa71d29a4663d87e9a
HughParsonage <hugh.parsonage@gmail.com>
2018-07-20 12:41:52 +1000

    Add examples, description to drop grep

--- R/drop_colr.R
+++ R/drop_colr.R
@@ -1,8 +1,18 @@
 #' Drop columns whose names match a pattern
 #' @param DT A \code{data.table}.
+#' @description \code{drop_colr} present since \code{hutils 1.0.0}.
+#' 
+#' \code{drop_grep} is identical but only present since \code{hutils 1.2.0}.
 #' @param pattern A regular expression as in \code{grepl}.
 #' @param ... Arguments passed to \code{grepl}.
 #' @param checkDT If \code{TRUE} (the default), will error if \code{DT} is not a \code{data.table}.
+#' 
+#' @examples
+#' library(data.table)
+#' dt <- data.table(x1 = 1, x2 = 2, y = 3)
+#' drop_grep(dt, "x")
+#' 
+#' 
 #' @export drop_colr drop_grep
 
 drop_colr <- function(DT, pattern, ..., checkDT = TRUE) {
