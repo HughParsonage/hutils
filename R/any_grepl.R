4a866c617ab8e2c25ec6262b742ff98b5fa45c41
HughParsonage <hugh.parsonage@gmail.com>
2018-08-09 21:37:57 +1000

    Initial commit of any_grepl, roxygen

---
+++ R/any_grepl.R
@@ -0,0 +1,47 @@
+#' Does the pattern appear anywhere?
+#' @description Shortcut for \code{any(grepl(...))}, mostly for consistency.
+#' @param x A character vector.
+#' @param pattern,perl,ignore.case,fixed As in \code{\link[base]{grep}}. 
+#' \code{invert} is simply 
+#' 
+#' @examples
+#' any_grepl(c("A_D_E", "K0j"), "[a-z]")
+#' 
+#' @export any_grepl
+#' 
+
+any_grepl <- function(x,
+                      pattern,
+                      perl = TRUE,
+                      ignore.case = FALSE,
+                      fixed = FALSE, 
+                      quiet = FALSE) {
+  if (fixed && ignore.case) {
+    if (!quiet) {
+      message("`fixed` and `ignore.case` both TRUE, ",
+              "so considering both upper and lowercase ",
+              "versions of pattern with fixed = TRUE.")
+    }
+    # For convenience
+    return(any_grepl(tolower(x),
+                     pattern = tolower(pattern), 
+                     perl = FALSE,
+                     ignore.case = FALSE,
+                     fixed = TRUE,
+                     expected = expected))
+  }
+  
+  if (missing(perl) && fixed) {
+    perl <- FALSE
+  }
+  
+  as.logical(length(grep(pattern = pattern,
+                         x = x,
+                         perl = perl,
+                         fixed = fixed,
+                         ignore.case = ignore.case)))
+  
+}
+
+
+
