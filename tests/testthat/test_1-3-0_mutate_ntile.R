a57b9605e8e2cda4b6d377b7d3eb52133429fb45
HughParsonage <hugh.parsonage@gmail.com>
2018-10-16 23:42:06 +1100

    Add coverage

--- tests/testthat/test_1-3-0_mutate_ntile.R
+++ tests/testthat/test_1-3-0_mutate_ntile.R
@@ -58,6 +58,21 @@ test_that("Error handling", {
   
 })
 
+test_that("character.only = TRUE error handling", {
+  library(data.table)
+  DT0 <- data.table(x = 1:200, y = rep(1:10, 20L))
+  expect_error(mutate_ntile(DT0, col = c("x", "y"), n = 5L, character.only = TRUE),
+               regexp = "`col` had length 2",
+               fixed = TRUE)
+  expect_error(mutate_ntile(DT0, col = 1, n = 5L, character.only = TRUE), 
+               regexp = "`col = 1` was a numeric",
+               fixed = TRUE)
+  expect_error(mutate_ntile(DT0, col = "sdf", n = 5L, character.only = TRUE),
+               regexp = "`col = sdf` but this is not a column in `DT`",
+               fixed = TRUE)
+  
+})
+
 test_that("character.only / NSE", {
   library(data.table)
   DT2 <- data.table(x = 1:200, y = rep(1:10, 20L))
