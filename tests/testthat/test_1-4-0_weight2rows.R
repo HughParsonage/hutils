7bfec8b011344acfef07591a3c36a39ae9366367
HughParsonage <hugh.parsonage@gmail.com>
2018-11-09 01:30:47 +1100

    Add coverage

--- tests/testthat/test_1-4-0_weight2rows.R
+++ tests/testthat/test_1-4-0_weight2rows.R
@@ -7,3 +7,11 @@ test_that("rows.out < 1", {
   expect_identical(weight2rows(DT, "y", rows.out = 0.1, discard_weight.var = TRUE),
                    DT[, .(x)])
 })
+
+test_that("weight includes 0", {
+  library(data.table)
+  DT <- data.table(x = 1:2,
+                   y = c(10L, 0L))
+  expect_equal(nrow(weight2rows(DT, "y")), 10L)
+})
+
