8fb2d46cc3c34c83df868274e3de6330ca986715
HughParsonage <hugh.parsonage@gmail.com>
2019-01-30 23:37:04 +1100

    Try different benchmarking verification

--- tests/testthat/test-backwards-compatibility.R
+++ tests/testthat/test-backwards-compatibility.R
@@ -4,13 +4,15 @@ test_that("1.0.0", {
   skip_if_not_installed("digest")
   skip_if_not(file.exists("test-backwards-compatibility.R"))
   tests_1.0.0 <- 
-    lapply(dir(pattern = "test_1.0.0.*R$"),
-           readLines,
-           # incomplete final line found on 'test_1-0-0-major-drop_colr.R'
-           warn = FALSE)
+    vapply(dir(pattern = "test.1.0.0.*\\.R$"),
+           tools::md5sum,
+           "") %>%
+    vapply(substr, 1L, 4L, FUN.VALUE = "") %>%
+    paste0(collapse = "")
   
-  expect_equal(digest::sha1(tests_1.0.0),
-               "098a575b17ff0629aa03bda1500e26abbc69dfd6")
+  
+  expect_equal(tests_1.0.0,
+               "fd5fc75ff8d63189c75fd05404b830fe66af5b718ee8e3c1b4d63dd8e2916ae51e594fd973e37590d3aa2113ed2bb276972144faa7f0")
     
 })
 
