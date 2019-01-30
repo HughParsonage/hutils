8baead8382c681bc499ce4251be17b061026962a
HughParsonage <hugh.parsonage@gmail.com>
2019-01-31 01:18:27 +1100

    Show DigestSha1

--- tests/testthat/test-backwards-compatibility.R
+++ tests/testthat/test-backwards-compatibility.R
@@ -2,6 +2,7 @@ context("Backwards compatible")
 
 test_that("1.0.0", {
   skip_if_not_installed("digest")
+  skip_if_not_installed("readr")
   skip_if_not(file.exists("test-backwards-compatibility.R"))
   tests_1.0.0 <- 
     vapply(dir(pattern = "test.1.0.0.*\\.R$"),
@@ -13,21 +14,17 @@ test_that("1.0.0", {
   
   if (!isTRUE(all.equal(tests_1.0.0,
                         "fd5fc75ff8d63189c75fd05404b830fe66af5b718ee8e3c1b4d63dd8e2916ae51e594fd973e37590d3aa2113ed2bb276972144faa7f0"))) {
-    print(vapply(dir(pattern = "test.1.0.0.*\\.R$"),
-                 tools::md5sum,
-                 ""))
-    print(lapply(lapply(dir(pattern = "test.1.0.0.*\\.R$")[1:4],
-                        readLines),
-                 function(x) x[nzchar(x)]))
-    print(vapply(vapply(lapply(lapply(dir(pattern = "test.1.0.0.*\\.R$")[1:4],
-                                      readLines),
-                               function(x) x[nzchar(x)]),
-                        digest::sha1,
-                        FUN.VALUE = ""),
-                 substr, 1L, 4L,
-                 FUN.VALUE = ""))
-    print(all.equal(tests_1.0.0,
-                    "fd5fc75ff8d63189c75fd05404b830fe66af5b718ee8e3c1b4d63dd8e2916ae51e594fd973e37590d3aa2113ed2bb276972144faa7f0"))
+    library(data.table)
+    tests_1.0.0.R <- dir(pattern = "test.1.0.0.*\\.R$")
+    DT <- data.table(File = tests_1.0.0.R)
+    setorderv(DT, "File")  # for C-locale ordering
+    DigestSha1 <- function(x) {
+      Lines <- readr::read_lines(x)
+      substr(digest::sha1(Lines[nzchar(Lines)]), 0, 8)
+    }
+    print(DT[, "DigestSha1" := DigestSha1(.BY[["File"]]), by = "File"])
+    
+    
   }
     
 })
