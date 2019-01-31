4594ac81e6e4ba1e0dc8af6de167ee8317af9379
HughParsonage <hugh.parsonage@gmail.com>
2019-01-31 20:33:03 +1100

    Add other backwards compatible tests

--- tests/testthat/test-backwards-compatibility.R
+++ tests/testthat/test-backwards-compatibility.R
@@ -1,42 +1,62 @@
 context("Backwards compatible")
 
-test_that("1.0.0", {
-  skip_if_not_installed("digest")
-  skip_if_not_installed("readr")
-  skip_if_not(file.exists("test-backwards-compatibility.R"))
-  tests_1.0.0 <- 
-    vapply(dir(pattern = "test.1.0.0.*\\.R$"),
-           tools::md5sum,
-           "") %>%
-    vapply(substr, 1L, 4L, FUN.VALUE = "") %>%
-    paste0(collapse = "")
-  
-  rm_trailing_empty <- function(x) {
-    while (!nzchar(x[length(x)])) {
-      x <- x[-length(x)]
-    }
-    x
+rm_trailing_empty <- function(x) {
+  while (!nzchar(x[length(x)])) {
+    x <- x[-length(x)]
   }
-  
+  x
+}
+
+createDigestSha1Tbl <- function(version = c("1.0.0", "1.1.0", "1.2.0", "1.3.0"),
+                                return_DT = FALSE) {
+  version <- match.arg(version)
   library(data.table)
-  tests_1.0.0.R <- dir(pattern = "test.1.0.0.*\\.R$")
-  DT <- data.table(File = tests_1.0.0.R)
-  setorderv(DT, "File")  # for C-locale ordering
   DigestSha1 <- function(x) {
     Lines <- readr::read_lines(x)  # encoding more consistent
     substr(digest::sha1(rm_trailing_empty(Lines)), 0, 8)
   }
+  Files <- dir(pattern = paste0("^test.", version, ".+\\.R$"))
+  DT <- data.table(File = Files)
   DT[, "DigestSha1" := DigestSha1(.BY[["File"]]), by = "File"]
   setcolorder(DT, c("DigestSha1", "File"))
-  expected <- fread("version-sha1s/v1-0-0.tsv", sep = "\t")
-  # Provide better diagnostics
-  if (!isTRUE(v100_equal <- all.equal(DT, expected))) {
-    print(v100_equal)
+  if (return_DT) {
+    return(DT[])
+  }
+  expected <- fread(dir(path = "version-sha1s",
+                        pattern = version,
+                        full.names = TRUE)[1],
+                    sep = "\t")
+  if (!isTRUE(unequal <- all.equal(DT, expected))) {
+    print(unequal)
     print(DT)
     print(expected)
-    expect_true(FALSE)
+    return(FALSE)
   }
-  expect_true(TRUE)
+  return(TRUE)
+}
+
+test_that("1.0.0", {
+  skip_if_not_installed("digest")
+  skip_if_not_installed("readr")
+  skip_if_not(file.exists("test-backwards-compatibility.R"))
+  
+  expect_true(createDigestSha1Tbl("1.0.0"))
+})
+
+test_that("1.1.0", {
+  skip_if_not_installed("digest")
+  skip_if_not_installed("readr")
+  skip_if_not(file.exists("test-backwards-compatibility.R"))
+  
+  expect_true(createDigestSha1Tbl("1.1.0"))
+})
+
+test_that("1.2.0", {
+  skip_if_not_installed("digest")
+  skip_if_not_installed("readr")
+  skip_if_not(file.exists("test-backwards-compatibility.R"))
+  
+  expect_true(createDigestSha1Tbl("1.2.0"))
 })
 
 test_that("1.1.0", {
