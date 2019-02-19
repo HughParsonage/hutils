461195c7c8cfa521db1ab57b98df897b9ff54e35
HughParsonage <hugh.parsonage@gmail.com>
2019-02-19 21:10:44 +1100

    Backport to R 3.2

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname = find.package("grattanReporter"), pkgname = "grattanReporter"){
-  
+  backports::import(pkgname)
   # CRAN Note avoidance
   if (getRversion() >= "2.15.1")
     utils::globalVariables(c("."))
