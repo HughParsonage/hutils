5bf93c8088bf5dcf056c244474e6491860187837
HughParsonage <hugh.parsonage@gmail.com>
2019-03-03 00:38:48 +1100

    Closes #29

---
+++ R/set_seed.R
@@ -0,0 +1,7 @@
+
+# Used to stabilize set.seed because of PR17494
+# and incremented r76165
+set.seed <- function(seed, kind = NULL, normal.kind = NULL) {
+  RNGversion("3.5.0")
+  base::set.seed(seed, kind = kind, normal.kind = normal.kind)
+}
