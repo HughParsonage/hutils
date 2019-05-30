bb30521835a03134a19a96608e3053d9ff638b3d
DaniWood <danielleiwood@gmail.com>
2019-05-30 22:51:14 +1000

    Avoid warnings from set.seed during test

--- R/set_seed.R
+++ R/set_seed.R
@@ -2,6 +2,6 @@
 # Used to stabilize set.seed because of PR17494
 # and incremented r76165
 set.seed <- function(seed, kind = NULL, normal.kind = NULL) {
-  RNGversion("3.5.0")
+  suppressWarnings(RNGversion("3.5.0"))
   base::set.seed(seed, kind = kind, normal.kind = normal.kind)
 }
